753b87e4d05cf35b5b494643dfae5a4e36bc7c8e
shrivarshan-c <shrivarshan.c2022ece@sece.ac.in>
2026-02-16 23:51:30 +0530

    error fixes

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
